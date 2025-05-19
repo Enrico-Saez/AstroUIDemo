5bdc3e48d6418ec7c254ef2453201334c7e5e548
Enrico Saez <enricoricsaez@gmail.com>
2025-05-19 19:16:32 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="text-center flex flex-col justify-center items-center h-screen">
+    <div class="text-5xl flex flex-col space-y-20 text-green-400 *:hover:bg-green-400 *:hover:text-slate-950 *:py-1 *:px-20">
+        <a href="/">New Game</a>
+        <a href="/">Continue</a>
+        <a href="/tutorial">Tutorial</a>
+        <a href="/settings">Settings</a>
+    </div>
+</div>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col justify-center items-center h-screen">
+    <div class="">
+        <p class="text-sky-400 text-5xl">Settings</p>
+        <p>Game</p>
+        <p>Sound</p>
+    </div>
+</div>
